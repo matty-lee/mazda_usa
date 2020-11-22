1e3419a60954fac5fb87afb687651ac84d3c85c0
matty-lee <matthew.b.lee2@gmail.com>
2020-11-21 20:51:09 -0800

    header transition

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ import { getElement } from './utils.js';
 const slides = document.querySelectorAll('.slide');
 const prevBtn = getElement('.prevBtn');
 const nextBtn = getElement('.nextBtn');
-
-slides.forEach(function (slide, index) {
-  slide.style.left = `${index * 100}%`;
-});
-
-let counter = 0;
+let slideCounter = 1;
 
 nextBtn.addEventListener('click', function () {
-  counter++;
-  carousel();
-});
-prevBtn.addEventListener('click', function () {
-  counter--;
-  carousel();
-});
-
-function carousel() {
-  if (counter == slides.length) {
-    counter = 0;
-  } else if (counter < 0) {
-    counter = slides.length - 1;
+  slideCounter++;
+  if (slideCounter > slides.length) {
+    slideCounter = 1;
   }
+  console.log(slideCounter);
+  slideTransition();
+});
 
+prevBtn.addEventListener('click', function () {
+  slideCounter--;
+  if (slideCounter < 1) {
+    slideCounter = slides.length;
+  }
+  console.log(slideCounter);
+  slideTransition();
+});
+
+function slideTransition() {
+  const currentSlide = getElement(`.slide:nth-child(${slideCounter}`);
   slides.forEach(function (slide) {
-    slide.style.transform = `translateX(-${counter * 100}%)`;
+    slide.classList.add('hide-slide');
   });
+  currentSlide.classList.remove('hide-slide');
 }
